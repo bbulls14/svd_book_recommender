f0ab87e305b6b8c5f90eb1fa9374be5ed8970fad
bbulls1 <bbulls14@gmail.com>
2024-09-21 18:04:48 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, render_template
 import pandas as pd
+import os
+import boto3
 import pickle
 
-# Load the trained model
-with open('svd_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
 
-# Load the dataset for recommendations
-books_df = pd.read_csv('joined_dataset.csv')
+def download_svd_model():
+    s3 = boto3.client('s3',
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION)
+    bucket_name = 'svdmodel'  
+    file_key = 'svd_model.pkl'
+    download_path = '/tmp/svd_model.pkl'
+
+    s3.download_file(bucket_name, file_key, download_path)
+
+    with open(download_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+model = download_svd_model()
+
+df = pd.read_csv('joined_dataset.csv')
 
 app = Flask(__name__)
 
@@ -17,55 +35,61 @@ def index():
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
-    # Get the book title from the user input
+    #get book that contains title input
     book_title = request.form['book_title']
-
-    # Find the ISBN of the book with partial match in title (case-insensitive)
-    book_data = books_df[books_df['Book-Title'].str.contains(book_title, case=False, na=False)].head(1)
+    book_data = df[df['Book-Title'].str.contains(book_title, case=False, na=False)].head(1)
 
     if book_data.empty:
         return render_template('index.html', message="Book not found!")
 
-    book_isbn = book_data['ISBN'].values[0]
-
-
-    # Get recommendations for similar users who liked the book
-    recommendations = get_book_recommendations(book_isbn)
-
-    print(recommendations)
+    #set userid to nonexistent user for predictions
+    user_id = 276723
     
-    return render_template('recommendations.html', books = recommendations)
+    
+    book_isbn = book_data['ISBN'].values[0]
+    recommendations = get_book_recommendations(user_id, book_isbn)
 
-def get_book_recommendations(book_isbn):
-    # Get users who rated the input book
-    similar_users = books_df[books_df['ISBN'] == book_isbn]['User-ID']
+    return render_template('recommendations.html', books=recommendations)
 
-    # Find other books these users have rated highly
-    recommended_books = books_df[books_df['User-ID'].isin(similar_users)]
+def get_book_recommendations(user_id, book_isbn):
+    
+    
+    #get books rated highly by users who rated book_isbn
+    high_rated_users = df[(df['ISBN'] == book_isbn) & (df['Book-Rating'] >= 8)]['User-ID'].unique()
 
-    # Filter books with ratings above 8
-    high_rated_books = recommended_books[recommended_books['Book-Rating'] > 8]
+    #get top 10 books from each user
+    top_books = []
+    for high_user in high_rated_users:
+        user_books = df[df['User-ID'] == high_user]
+        top_user_books = user_books.sort_values(by='Book-Rating', ascending=False).head(10)
+        top_books.extend(top_user_books[['ISBN', 'Book-Title', 'Book-Author']].values.tolist())
+    
+    #remove duplicate books using isbn
+    seen_isbns = set()
+    unique_books = []
+    for book in top_books:
+        if book[0] not in seen_isbns:
+            seen_isbns.add(book[0])
+            unique_books.append(book)
+            
+    #predict rating for books
+    books_with_predictions = []
+    for book in unique_books:
+        prediction = model.predict(user_id, book[0]).est  
+        books_with_predictions.append((book[1], book[2], prediction))
 
-    # Count the number of users who rated each book above 8 (popularity)
-    book_popularity = high_rated_books.groupby('ISBN').size().reset_index(name='num_ratings_above_8')
+    sorted_books = sorted(books_with_predictions, key=lambda x: x[2], reverse=True)
 
-    # Merge popularity with original book data to include titles and ratings
-    merged_books = pd.merge(high_rated_books, book_popularity, on='ISBN')
+    #keep top 5
+    top_books = sorted_books[:5]
 
-    # Sort by popularity (number of ratings above 8) and then by book rating
-    top_books = merged_books.sort_values(by=['num_ratings_above_8', 'Book-Rating'], ascending=[False, False])
-
-    filtered_books = top_books[top_books['ISBN'] != book_isbn]
-
-    # Drop duplicate titles to ensure only unique titles are recommended
-    unique_books = filtered_books.drop_duplicates(subset='Book-Title')
-
-    # Convert DataFrame to list of dictionaries, ensuring only unique titles
-    book_list = unique_books[['Book-Title', 'Book-Author']].head(5).to_dict(orient='records')
+    #use list of dicts so HTML can access items
+    book_list = [{'Book-Title': title, 'Book-Author': author} for title, author, _ in top_books]
 
     return book_list
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
